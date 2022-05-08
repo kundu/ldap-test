89fb464737215332ea05cdee39a81722124b3618
Sauvik Kundu <sauvik.kundu@sslwireless.com>
2022-05-08 20:47:45 +0600

    ldap addd

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/user-create', [Controller::class, 'userCreate']);
+Route::get('/', [Controller::class, 'userCreate']);
