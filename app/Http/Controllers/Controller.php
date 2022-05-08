c3fd65e36f9d6144f0063c2cc279b979fc56eb38
Sauvik Kundu <sauvik.kundu@sslwireless.com>
2022-05-08 20:45:59 +0600

    ldap addd

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Ldap\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,16 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function userCreate(){
+        $user = new User();
+
+        $user->cn = 'Steve Bauman';
+        $user->givenname = 'Steve';
+        $user->sn = 'Bauman';
+        $user->company = 'Acme';
+        $user->save();
+
+        dd($user);
+    }
 }
